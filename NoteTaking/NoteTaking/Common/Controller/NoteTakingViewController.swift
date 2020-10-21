eb0ae7efea80d5118aed8154858fc8af015c2818
kaden-kykim <kaden.kykim@gmail.com>
2020-10-21 15:20:42 -0700

    Boilerplate

---
+++ NoteTaking/NoteTaking/Common/Controller/NoteTakingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTakingViewController.swift
+//  NoteTaking
+//
+//  Created by Kaden Kim on 2020-10-21.
+//
+
+import UIKit
+
+class NoteTakingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+}
